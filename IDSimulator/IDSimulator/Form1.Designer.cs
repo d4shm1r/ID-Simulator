20e2255b68a70f5c0304d8c941858211cc86b89e
Dashmir Mejdi <58791115+d4shm1r@users.noreply.github.com>
2021-07-11 04:05:54 +0200

    Add Form App

---
+++ IDSimulator/IDSimulator/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Guess_Your_ID {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Day = new System.Windows.Forms.TextBox();
+            this.Month = new System.Windows.Forms.TextBox();
+            this.Year = new System.Windows.Forms.TextBox();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.Birthday = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Birthday.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Day
+            // 
+            this.Day.Location = new System.Drawing.Point(15, 48);
+            this.Day.Name = "Day";
+            this.Day.Size = new System.Drawing.Size(100, 20);
+            this.Day.TabIndex = 0;
+            // 
+            // Month
+            // 
+            this.Month.Location = new System.Drawing.Point(139, 48);
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(100, 20);
+            this.Month.TabIndex = 1;
+            // 
+            // Year
+            // 
+            this.Year.Location = new System.Drawing.Point(270, 48);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(100, 20);
+            this.Year.TabIndex = 2;
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Location = new System.Drawing.Point(99, 104);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(48, 17);
+            this.Male.TabIndex = 3;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Location = new System.Drawing.Point(224, 104);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(59, 17);
+            this.Female.TabIndex = 4;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // Birthday
+            // 
+            this.Birthday.Controls.Add(this.label3);
+            this.Birthday.Controls.Add(this.label2);
+            this.Birthday.Controls.Add(this.label1);
+            this.Birthday.Controls.Add(this.Day);
+            this.Birthday.Controls.Add(this.Female);
+            this.Birthday.Controls.Add(this.Month);
+            this.Birthday.Controls.Add(this.Male);
+            this.Birthday.Controls.Add(this.Year);
+            this.Birthday.Location = new System.Drawing.Point(12, 12);
+            this.Birthday.Name = "Birthday";
+            this.Birthday.Size = new System.Drawing.Size(396, 154);
+            this.Birthday.TabIndex = 5;
+            this.Birthday.TabStop = false;
+            this.Birthday.Text = "Birthday";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate ID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Day";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(296, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Year";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 232);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Birthday);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Birthday.ResumeLayout(false);
+            this.Birthday.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Day;
+        private System.Windows.Forms.TextBox Month;
+        private System.Windows.Forms.TextBox Year;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.GroupBox Birthday;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ IDSimulator/IDSimulator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_Your_ID {
+    public partial class Form1 : Form {
+        /*
+         * This form application is a simulated program
+         * that should use some basic info and create a
+         * unique ID that should be used for citizens
+         * older than 18 years old
+         */
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Ask User for Day, Month and Year
+            string day = Day.Text;
+            string month = Month.Text;
+            string year = Year.Text;
+
+            // Generate random 3 digit number
+            Random rdm = new Random();
+            int rdmNum = rdm.Next(9, 999);
+
+            // Toggle Gender RadioButton
+            if (Male.Checked) {
+                if (rdmNum < 100) {
+                    textBox1.Text = day + month + year.Remove(0, 1) + "450" + "0" + Convert.ToString(rdmNum);
+                } else {
+                    textBox1.Text = day + month + year.Remove(0, 1) + "450" + Convert.ToString(rdmNum);
+                }
+            }
+            else if (Female.Checked) {
+                if (rdmNum < 100) {
+                    textBox1.Text = day + month + year.Remove(0, 1) + "455" + "0" + Convert.ToString(rdmNum);
+                }
+                else {
+                    textBox1.Text = day + month + year.Remove(0, 1) + "455" + Convert.ToString(rdmNum);
+                }
+            }
+            else {
+                textBox1.Text = "";
+            }
+        }
+    }
+}
